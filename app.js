339dfb6dee704785422401e2431414a23b705398
Sambit-art <63838832+Sambit-art@users.noreply.github.com>
2022-09-11 01:30:02 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,26 @@
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://test:test123@demo.2isk9.mongodb.net/?retryWrites=true&w=majority"
+const express = require('express')
+const app=express()
+
+app.get('/data', (req, res) => {
+    
+    async function data() {
+        const client = await MongoClient.connect(uri, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        // specify the DB's name
+        const db = client.db('demodb');
+        // execute find query
+        const items = await db.collection('users').find({}).toArray();
+        client.close();
+        return items
+    }
+    data().then((data)=>{res.json(data)})
+})
+
+app.listen(3000, () => {
+    console.log('server started at port 3000');
+})
